c51111b211a9c9db1e1f50cffa8fb81979a5416d
Blackflame-Queen <blackflamearia@gmail.com>
2025-01-07 15:49:56 -0800

    update

---
+++ Node.js
@@ -0,0 +1,10 @@
+import { initializeKinesis } from "@amineyarman/kinesis";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeKinesis();
+});
+import { initializeKinesis } from "@amineyarman/kinesis";
+
+document.addEventListener("DOMContentLoaded", () => {
+  initializeKinesis();
+});
