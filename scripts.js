606dfcde39e56e68ec0b8cd40f19c44839bb9435
Blackflame-Aria <blackflameit@gmail.com>
2025-01-22 00:09:10 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,51 @@
+const bakuganBattleBrawlers = [
+    {
+        name: "Dan Kuso",
+        age: 12,
+        definingTrait: "Audacity",
+        bakugan: "Dragonoid",
+        element: "Pyrus",
+        evolutions: ["Delta Dragonoid", "Ultimate Dragonoid", "Infinity Dragonoid"]
+    },
+    {
+        name: "Runo Misaki",
+        age: 12,
+        definingTrait: "Rectitude",
+        bakugan: "Tigrerra",
+        element: "Haos",
+        evolution: "Blade Tigrerra",
+    },
+    {
+        name: "Julie Makimoto",
+        age: 12,
+        definingTrait: "Tenacity",
+        bakugan: "Gorem",
+        element: "Subterra",
+        evolution: "Hammer Gorem",
+    },
+    {
+        name: "Alice Gehabich",
+        age: 14,
+        definingTrait: "Guile",
+        bakugan: "Hydranoid",
+        element: "Darkus",
+        evolution: "Alpha Hydranoid",
+    },
+    {
+        name: "Shun Kazami",
+        age: 13,
+        definingTrait: "Cunning",
+        bakugan: "Skyress",
+        element: "Ventus",
+        evolution: "Storm Skyress",
+    },
+    {
+        name: "Marucho Marukura",
+        age: 11,
+        definingTrait: "Intelligence",
+        bakugan: "Preyas",
+        element: "Aquos",
+        evolution: "Angelo/Diablo Preyas",
+    }
+];
+console.log(bakuganBattleBrawlers)
